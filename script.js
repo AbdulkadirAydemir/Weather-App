d4ce16c804521ece425caa60e6422a90c160c44b
Abdulkadir Aydemir <111640113+AbdulkadirAydemir@users.noreply.github.com>
2023-04-23 18:53:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,31 @@ locationBtn.addEventListener("click", () =>{
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }else{
-        alert("Tarayıcınız coğrafi konumu api'yi desteklemiyor");
+        alert("Tarayıcınızın coğrafi konumu api'yi desteklemiyor");
     }
 });
+
+function requestApi(city){
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=your_api_key`;
+    fetchData();
+}
+
+function onSuccess(position){
+    const {latitude, longitude} = position.coords;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=your_api_key`;
+    fetchData();
+}
+
+function onError(error){
+    infoTxt.innerText = error.message;
+    infoTxt.classList.add("error");
+}
+
+function fetchData(){
+    infoTxt.innerText = "Hava durumu ayrıntıları alınıyor...";
+    infoTxt.classList.add("pending");
+    fetch(api).then(res => res.json()).then(result => weatherDetails(result)).catch(() =>{
+        infoTxt.innerText = "Bir şeyler yanlış gitti";
+        infoTxt.classList.replace("pending", "error");
+    });
+}
